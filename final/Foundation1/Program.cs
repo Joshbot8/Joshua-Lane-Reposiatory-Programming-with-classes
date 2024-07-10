d6edee2baffdad36e7219d171e0458aa65a5d542
Joshbot8 <hoppingamimations105@gmail.com>
2024-07-09 22:08:23 -0600

    Almost done with #1 :D

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,25 +1,58 @@
 using System;
 
+
+
+
 //main
 class Program ()
 {
-    
+   
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+
+
         List<Video> _videoList = new List<Video>();
+
+
+        string localTitle = "Title 1";
+        string localAuthor = "Author 1";
+        int localLength = 0;
+
+
+
+        Video video1 = new Video(localTitle, localAuthor, localLength);
+        string _commentAuthor = "CommentAuthor1";
+        string _commentText = "CommentText1";
+
+        video1.AddComment(_commentAuthor, _commentText);
+
+        video1.AddComment("Howdy im the second author","I am the text :D");
+
+
+        _videoList.Add(video1);
+       
+       int temp = _videoList.Count();
+
+       video1.GetNumberOfComments();
+
+       Console.WriteLine("Comments ("+ video1.GetNumberOfComments() +")");
     }
-
 }
-
     class Video
     {
         // Attributes
-        List<Comment> _comments = new List<Comment>();
+        List<Comment> _commentsList = new List<Comment>();
+
+
+
 
         private string _title ;
         private string _author ;
         private int _length ;
+
+
+
 
         public string Title
         {
@@ -39,37 +72,55 @@ class Program ()
 
 
         // Constructor
-        public Video(string title, string description, string author, int length)
+        public Video(string title, string author, int length)
         {
             _title = title;
             _author = author;
             _length  =  length; //length of the video;
         }
 
+
+
+
         // Methods
         public void AddComment(string name, string text)
         {
-           _comments.Add(new Comment(name, text));
+           _commentsList.Add(new Comment(name, text));
         }
+
+
+
 
         public void DisplayVideoInfoAndComments()
         {
             //TBD
         }
 
-        public void GetNumberOfComments()
+
+
+
+        public int GetNumberOfComments()
         {
-            //TBD
+            return _commentsList.Count;
         }
 
-        
+
+
+
+       
     }
+
+
+
 
     class Comment
     {
         // Attributes
         private string _commentAuthor ;
         private string _commentText ;
+
+
+
 
         public string Name
         {
@@ -83,6 +134,12 @@ class Program ()
         }
 
 
+
+
+
+
+
+
         // Methods
         public Comment( string name, string text)
         {
@@ -90,9 +147,11 @@ class Program ()
            _commentText = text;
         }
 
+
+
         public void DisplayComment()
         {
-           // TBD
+           Console.WriteLine( _commentAuthor + ": " + _commentText);
         }
 
      }
